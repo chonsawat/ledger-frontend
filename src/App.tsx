bd360f36dbeaf882988a07ff27a0e9de7540aec2
Chonsawat Nakanam <chonsawat.nakanam@kkumail.com>
2025-11-06 20:28:21 +0700

    add tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { createBrowserRouter, RouterProvider } from "react-router"
-import './App.css'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 
 import MainRouter from "./router/MainRouter"
+import './App.css'
 
 const router = createBrowserRouter(MainRouter)
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <RouterProvider router={router}></RouterProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RouterProvider router={router}></RouterProvider>
+    </QueryClientProvider>
   )
 }
 
